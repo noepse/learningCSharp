7ce332f61bc68d573b5610211caeffb3a913837a
simmi <22905246+noepse@users.noreply.github.com>
2024-04-05 16:09:46 +0100

    add nested conditional foreach practice

--- Program.cs
+++ Program.cs
@@ -1,18 +1,94 @@
-﻿
+﻿using System;
 
-string str = "The quick brown fox jumps over the lazy dog.";
-char[] charMessage = str.ToCharArray();
+// initialize variables - graded assignments 
+int examAssignments = 5;
 
-Array.Reverse(charMessage);
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
-int x = 0;
-foreach (char i in charMessage) { 
-    if (i == 'o') { 
-        x++; 
-        } 
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
+
+Console.WriteLine("Student\t\tGrade\n");
+
+int gradedAssignments = 0;
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+    {
+        studentScores = sophiaScores;
     }
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
 
-string new_message = new String(charMessage);
+    int sum = 0;
+    gradedAssignments = 0;
+    foreach (int score in studentScores)
+    {
+        gradedAssignments += 1;
+        if (gradedAssignments <= examAssignments)
+            // add the exam score to the sum
+            sum += score;
 
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+        else
+            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+            sum += score / 10;
+
+    }
+    decimal currentStudentGrade = 0;
+    currentStudentGrade = (decimal)(sum) / examAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+}
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71b44b599f50b056036bdaaf8e60a8bf8a19ee13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5872fde8e6e0a1af74965ba08788b530b8f15026")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
