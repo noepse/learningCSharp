5872fde8e6e0a1af74965ba08788b530b8f15026
simmi <22905246+noepse@users.noreply.github.com>
2024-04-05 15:31:42 +0100

    practice whitespacing

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
-﻿string[] orderNumbers = {"B123",
-"C234",
-"A345",
-"C15",
-"B177",
-"G3003",
-"C235",
-"B179" };
+﻿
 
-foreach (string item in orderNumbers)
-{
-    if (item.StartsWith("B")){
-Console.WriteLine(item);
+string str = "The quick brown fox jumps over the lazy dog.";
+char[] charMessage = str.ToCharArray();
+
+Array.Reverse(charMessage);
+
+int x = 0;
+foreach (char i in charMessage) { 
+    if (i == 'o') { 
+        x++; 
+        } 
     }
-}
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
