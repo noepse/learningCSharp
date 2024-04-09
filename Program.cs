169ee6c0feffeabc1765e380bbdf922ad1df69e0
simmi <22905246+noepse@users.noreply.github.com>
2024-04-09 14:48:45 +0100

    edit cast challenge solution

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ float value3 = 4.3f;
 
 // Your code here to set result1
 // Hint: You need to round the result to nearest integer (don't just truncate)
-int result1 =  value1 / (int) value2;
+int result1 = Convert.ToInt32(value1 / value2);
 Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
 // Your code here to set result2
@@ -12,5 +12,5 @@ decimal result2 = value2/ (decimal)value3;
 Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
 // Your code here to set result3
-float result3 = (float)(value3 / value1);
+float result3 = value3 / value1;
 Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
