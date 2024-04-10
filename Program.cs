38f314a1b13f6316a301d6933a643e9d1df752e2
simmi <22905246+noepse@users.noreply.github.com>
2024-04-10 15:22:38 +0100

    add roll game

--- Program.cs
+++ Program.cs
@@ -1,26 +1,38 @@
-﻿string[,] corporate = 
+﻿Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay()) 
 {
-    {"Robert", "Bavin"}, {"Simon", "Bright"},
-    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
-    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
-};
-
-string[,] external = 
-{
-    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
-    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
-};
-string companyDomain = "contoso.com";
-string externalDomain = "hayworth.com";
-
-getEmails(corporate, true);
-getEmails(external, false);
-
-void getEmails(string[,] names, bool isCorporate){
-
-        for (int i = 0; i < names.GetLength(0); i++) 
-{
- string email = corporate[i, 0].Substring(0, 2).ToLower() + corporate[i, 1].ToLower() + "@" + (isCorporate? companyDomain : externalDomain);
- Console.WriteLine(email);
+    PlayGame();
 }
+
+bool ShouldPlay(){
+    string? readline = Console.ReadLine();
+    if (readline.ToLower() == "y"){
+        return true;
     }
+    else return false;
+}
+
+string WinOrLose(int target, int roll){
+    if (roll > target){
+        return "You win!";
+    } else return "You lose!";
+}
+
+void PlayGame() 
+{
+    var play = true;
+
+    while (play) 
+    {
+        int target = random.Next(1, 6);
+        int roll = random.Next(1, 6);
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(target, roll));
+        Console.WriteLine("\nPlay again? (Y/N)");
+        play = ShouldPlay();
+    }
+}
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6757f312dca65696347de3e8ed10b9a77b86d7b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207c790b655159296d65d74054c7c0989d4abc6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
