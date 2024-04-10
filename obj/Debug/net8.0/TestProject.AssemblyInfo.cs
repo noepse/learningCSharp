207c790b655159296d65d74054c7c0989d4abc6d
simmi <22905246+noepse@users.noreply.github.com>
2024-04-10 14:57:37 +0100

    add get emails

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+191c0345d6faae1aa817423cfd4cfae818bbe133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6757f312dca65696347de3e8ed10b9a77b86d7b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
