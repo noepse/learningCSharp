b6eca9b814b36ad83329f289fa8fc15c19deaf1f
simmi <22905246+noepse@users.noreply.github.com>
2024-04-10 16:20:16 +0100

    add game challenge

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207c790b655159296d65d74054c7c0989d4abc6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f314a1b13f6316a301d6933a643e9d1df752e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
